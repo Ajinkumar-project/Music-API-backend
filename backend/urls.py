2289930e7fc3ba685663c70ec33a0e806149a0af
Ajinkumar-project <ajinkumar.s2004@gmail.com>
2026-08-09 21:07:02 +0530

    Documantation Added

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/docs/', TemplateView.as_view(template_name='api_docs.html'), name='api-docs'),
     path('api/', include('music.urls')),
 ]
